386bf7cbfa438d16a263ff4412a460f8c1340476
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-22 14:23:59 -0700

    Add files via upload

---
+++ Q36.js
@@ -0,0 +1,10 @@
+"use strict";
+// 36. T-shirts: Write a function called make_shirt() that accepts a size and the text of a message that should be 
+// printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message 
+// printed on it. Call the function.
+function makeShirt(size, text) {
+    console.log(`you order a ${size} shirt with print ${text}`);
+}
+makeShirt('large', '"But Coffee First"');
+makeShirt('medium', '"Awaken The Legacy"');
+makeShirt('small', '"Together Is Better"');
